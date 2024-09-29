260fe8020fdb696cc7fc646c445781d4c851184b
Hans Boder <36736535+hansboder@users.noreply.github.com>
2024-09-29 11:13:48 -0400

    Import fix

--- dags/kube_task.py
+++ dags/kube_task.py
@@ -1,17 +1,17 @@
 from airflow import DAG
-from airflow.operators.kubernetes_pod_operator import KubernetesPodOperator
+from airflow.providers.cncf.kubernetes.operators.pod import KubernetesPodOperator
 from airflow.utils.dates import days_ago
 
 default_args = {
     'owner': 'airflow',
 }
 
-with DAG('kube_task',
+with DAG(
+    dag_id='kube_task',
     default_args=default_args,
     schedule_interval=None,
     start_date=days_ago(1),
-    tags=['kubernetes-task'], 
-    catchup=False
+    tags=['kubernetes-task'],
 ) as dag:
 
     kube_operator = KubernetesPodOperator(
