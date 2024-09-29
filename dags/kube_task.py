18e6f7d69a03a50836719a175eba988f2ccd5739
Hans Boder <36736535+hansboder@users.noreply.github.com>
2024-09-29 10:00:53 -0400

    example k8s dag

---
+++ dags/kube_task.py
@@ -0,0 +1,25 @@
+from airflow import DAG
+from airflow.operators.kubernetes_pod_operator import KubernetesPodOperator
+from airflow.utils.dates import days_ago
+
+default_args = {
+    'owner': 'airflow',
+}
+
+with DAG(
+    dag_id='kube_task',
+    default_args=default_args,
+    schedule_interval=None,
+    start_date=days_ago(1),
+    tags=['kubernetes-task'],
+) as dag:
+
+    kube_operator = KubernetesPodOperator(
+        namespace='airflow',
+        image="bash:devel-alpine3.20",
+        cmds=["bash", "-cx"],
+        arguments=["echo", "10"],
+        name="echo",
+        task_id="echo",
+        get_logs=True
+    )
