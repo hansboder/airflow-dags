0191be32d3a5062e3969d7e23de2e12fc6c0f066
Hans Boder <36736535+hansboder@users.noreply.github.com>
2024-10-27 15:58:44 -0400

    File reader dag

---
+++ dags/file_reader.py
@@ -0,0 +1,32 @@
+from airflow import DAG
+from airflow.utils.dates import days_ago
+
+default_args = {
+    'owner': 'airflow',
+}
+
+
+with DAG(
+    dag_id='file_reader',
+    default_args=default_args,
+    schedule_interval=None,
+    start_date=days_ago(1),
+    tags=['file-reader'],
+    catchup=False
+) as dag:
+
+        
+    @dag.task
+    def list_files(**context):
+        import os
+        data_dir = "/data"
+        csv_files = [f for f in os.listdir(data_dir) if f.endswith('.csv')]
+        if csv_files:
+            for csv_file in csv_files:
+                context['ti'].log.info(f"found file: {csv_file}")
+        else:
+            context['ti'].log.info("no csv files found")
+
+
+
+    list_files()
